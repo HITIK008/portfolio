1a7bdd36525c726aaf982eeff155b8fa54e3a21f
Hitik Patel <hitik@Hitiks-MacBook-Air.local>
2024-06-12 12:07:12 +0530

    hwlli world

---
+++ src/content_option.js
@@ -0,0 +1,188 @@
+const logotext = "HP";
+const meta = {
+    title: "Hitik Patel",
+    description: "I’m Hitik Patel a Full stack devloper, I am student of Nirma University",
+};
+
+const introdata = {
+    title: "I’m Hitik Patel",
+    animated: {
+        first: "I am a Problem Solver",
+        second: "I am a Developer",
+        third: "I am an Avid Learner",    
+    },
+    description: "As a full-stack software engineer and entrepreneur, I possess expertise in developing large-scale web applications and backend systems. I am proficient in various programming languages, frameworks, and technologies. I am driven and enthusiastic about programming, from designing system architecture to exploring frameworks in depth and solving complex problems.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am Hitik Patel, I am pursuing a B.Tech from Nirma University, I am a full stack Web-Developer, I have leadership skills and manage clients effectively, I am from surat, Gujarat, INDIA",
+};
+const worktimeline = [{
+        jobtitle: "Web-Developer",
+        where: "DURVASA Infotech (SURAT)",
+        date: "2020-2021",
+    },
+   
+];
+
+const skills = [{
+        name: "HTML5",
+        value: 90,
+    },
+    {
+        name: "CSS2",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Node js",
+        value: 75,
+    },
+    {
+        name: "MongoDb",
+        value: 80,
+    },
+    {
+        name: "Ui-Ux Design",
+        value: 90,
+    },
+    {
+        name: "Bootstarp/tailwind",
+        value: 80,
+    },
+    {
+        name: "Mysql/DBMS",
+        value: 85,
+    },
+    {
+        name: "Leadership",
+        value: 100,
+    },
+    {
+        name: "Docker",
+        value: 50,
+    },
+    {
+        name: "AWS",
+        value: 70,
+    },
+    {
+        name: "AI/ML",
+        value: 75,
+    },
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "As a client manager, I provide quick and relevant content that is useful and fulfills your requirements.",
+    },
+    {
+        title: "Websites Design",
+        description: "As a client manager, I provide quick and relevant content that is useful and fulfills your requirements.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "As a client manager, I provide quick and relevant content that is useful and fulfills your requirements.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/0ZSKth1/Screenshot-2024-06-12-at-11-52-47-AM.png",
+        description: "Shoping website which is made in react",
+        link: "https://shoping-women.onrender.com/",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "under process",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hitikpatel13@gmail.com",
+    YOUR_FONE: "+91 7016737043",
+    description: " For further inquiries, please contact me at [my Email]. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/hitik008",
+    linkedin: "https://www.linkedin.com/in/hitik-patel-55aa40245/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
